59e4dcbfe2a7b80f4613104c1fc84a8c6701de27
sirmoncef <moncefzabat@gmail.com>
2025-04-13 11:06:47 +0000

    fix views logique

--- Backend/products/views.py
+++ Backend/products/views.py
@@ -17,26 +17,6 @@ class CategoryAPIView(APIView):
             serializer = CategorySerializer(categories, many=True)
         return Response(serializer.data)
 
-    def post(self, request):
-        serializer = CategorySerializer(data=request.data)
-        if serializer.is_valid():
-            serializer.save()
-            return Response(serializer.data, status=status.HTTP_201_CREATED)
-        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
-
-    def put(self, request, pk):
-        category = get_object_or_404(Category, pk=pk)
-        serializer = CategorySerializer(category, data=request.data, partial=True)
-        if serializer.is_valid():
-            serializer.save()
-            return Response(serializer.data)
-        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
-
-    def delete(self, request, pk):
-        category = get_object_or_404(Category, pk=pk)
-        category.delete()
-        return Response({"message": "Category deleted successfully."}, status=status.HTTP_204_NO_CONTENT)
-
 
 class BrandAPIView(APIView):
     def get(self, request, pk=None):
@@ -48,25 +28,7 @@ class BrandAPIView(APIView):
             serializer = BrandSerializer(brands, many=True)
         return Response(serializer.data)
 
-    def post(self, request):
-        serializer = BrandSerializer(data=request.data)
-        if serializer.is_valid():
-            serializer.save()
-            return Response(serializer.data, status=status.HTTP_201_CREATED)
-        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
-
-    def put(self, request, pk):
-        brand = get_object_or_404(Brand, pk=pk)
-        serializer = BrandSerializer(brand, data=request.data, partial=True)
-        if serializer.is_valid():
-            serializer.save()
-            return Response(serializer.data)
-        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
-
-    def delete(self, request, pk):
-        brand = get_object_or_404(Brand, pk=pk)
-        brand.delete()
-        return Response({"message": "Brand deleted successfully."}, status=status.HTTP_204_NO_CONTENT)
+    
 
 
 class ProductAPIView(APIView):
@@ -99,25 +61,7 @@ class ProductAPIView(APIView):
         serializer = ProductSerializer(products, many=True)
         return Response(serializer.data)
 
-    def post(self, request):
-        serializer = ProductSerializer(data=request.data)
-        if serializer.is_valid():
-            serializer.save()
-            return Response(serializer.data, status=status.HTTP_201_CREATED)
-        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
-
-    def put(self, request, pk):
-        product = get_object_or_404(Product, pk=pk)
-        serializer = ProductSerializer(product, data=request.data, partial=True)
-        if serializer.is_valid():
-            serializer.save()
-            return Response(serializer.data)
-        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
-
-    def delete(self, request, pk):
-        product = get_object_or_404(Product, pk=pk)
-        product.delete()
-        return Response({"message": "Product deleted successfully."}, status=status.HTTP_204_NO_CONTENT)
+    
 
 
 class AttributeAPIView(APIView):
@@ -130,25 +74,7 @@ class AttributeAPIView(APIView):
             serializer = AttributeSerializer(attributes, many=True)
         return Response(serializer.data)
 
-    def post(self, request):
-        serializer = AttributeSerializer(data=request.data)
-        if serializer.is_valid():
-            serializer.save()
-            return Response(serializer.data, status=status.HTTP_201_CREATED)
-        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
-
-    def put(self, request, pk):
-        attribute = get_object_or_404(Attribute, pk=pk)
-        serializer = AttributeSerializer(attribute, data=request.data, partial=True)
-        if serializer.is_valid():
-            serializer.save()
-            return Response(serializer.data)
-        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
-
-    def delete(self, request, pk):
-        attribute = get_object_or_404(Attribute, pk=pk)
-        attribute.delete()
-        return Response({"message": "Attribute deleted successfully."}, status=status.HTTP_204_NO_CONTENT)
+    
 
 
 class ProductDetailAPIView(APIView):
@@ -161,27 +87,9 @@ class ProductDetailAPIView(APIView):
             serializer = ProductDetailSerializer(product_details, many=True)
         return Response(serializer.data)
 
-    def post(self, request):
-        serializer = ProductDetailSerializer(data=request.data)
-        if serializer.is_valid():
-            serializer.save()
-            return Response(serializer.data, status=status.HTTP_201_CREATED)
-        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+    
 
-    def put(self, request, pk):
-        product_detail = get_object_or_404(ProductDetail, pk=pk)
-        serializer = ProductDetailSerializer(product_detail, data=request.data, partial=True)
-        if serializer.is_valid():
-            serializer.save()
-            return Response(serializer.data)
-        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
-
-    def delete(self, request, pk):
-        product_detail = get_object_or_404(ProductDetail, pk=pk)
-        product_detail.delete()
-        return Response({"message": "Product detail deleted successfully."}, status=status.HTTP_204_NO_CONTENT)
-
-
+   
 class AvailableProductDetailAPIView(APIView):
     def get(self, request):
         available_products = ProductDetail.objects.filter(stock__gt=0)
